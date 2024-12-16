1ffaf426bfb358070fb70e07397d401e700337eb
GMchalana <gmchalanaprabhashwara@gmail.com>
2024-12-16 17:29:05 +0530

    Add the theme changing funtion

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,34 @@
-// @type {import('tailwindcss').Config}
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryMainDark: '#030F16',
+        primaryMainLight: '#F4F5F6',
+        headerDark: '#072435',
+        headerLight: '#FFFFFF',
+        sideNavDark: '#072435',
+        sideNavLight: '#FFFFFF',
+        tileBlack: '#061E2C',
+        tileLight: '#FFFFFF',
+        tileHoverDark: '#14557C',
+        tileHoverLight: '#e9e9e9',
+        iconCoverDark: '#123651',
+        iconCoverLight: '#CDDFEF66',
+        textDark: '#C4C4C4',
+        textLight: '#534E4E',
+        navText:'#061E2C',
+        darkModeBg: '#1a202c', // Custom background color for dark mode
+        darkModeText: '#DFDFDF',
+        lightModeText: '#a0aec0', // Custom text color for dark mode
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar'),
+  ],
 }

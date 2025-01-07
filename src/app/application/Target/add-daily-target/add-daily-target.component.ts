0b8ca3df33d97882b04b15e5ef85d12c5474d68a
Pasan-FDo <pasanfernando.work@gmail.com>
2025-01-07 11:15:11 +0530

    feat: add AddDailyTargetComponent with template, styles, and tests

---
+++ src/app/application/Target/add-daily-target/add-daily-target.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-daily-target',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-daily-target.component.html',
+  styleUrl: './add-daily-target.component.css'
+})
+export class AddDailyTargetComponent {
+
+}

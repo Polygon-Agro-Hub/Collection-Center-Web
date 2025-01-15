3f385276c8d18f5b79c2f241a6c3eac163d13371
Pasan-FDo <pasanfernando.work@gmail.com>
2025-01-15 11:13:30 +0530

    feat: add OfficerProfileComponent with initial template, styles, and tests

---
+++ src/app/application/manage-officers/officer-profile/officer-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-officer-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './officer-profile.component.html',
+  styleUrl: './officer-profile.component.css'
+})
+export class OfficerProfileComponent {
+
+}

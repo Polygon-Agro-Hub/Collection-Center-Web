15f0df4b7e3d3d455b2377371eb350a8f1040146
Pasan-FDo <pasanfernando.work@gmail.com>
2025-01-16 11:45:20 +0530

    feat: enhance DownloadTargetComponent with date range selection and data display

--- src/app/application/Target/download-target/download-target.component.ts
+++ src/app/application/Target/download-target/download-target.component.ts
@@ -1,12 +1,69 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { TargetService } from '../../../services/Target-service/target.service';
 
 @Component({
   selector: 'app-download-target',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './download-target.component.html',
   styleUrl: './download-target.component.css'
 })
 export class DownloadTargetComponent {
+  targetArr!: DailyTargets[];
 
+  fromDate: Date | string = '';
+  toDate: Date | string = '';
+
+  hasData: boolean = false;
+
+  constructor(
+    private router: Router,
+    private TargetSrv: TargetService
+  ) { }
+
+
+  fetchDownloadTarget() {
+    this.TargetSrv.downloadDailyTarget(this.fromDate, this.toDate).subscribe(
+      (res) => {
+        if (res.status) {
+          this.targetArr = res.data
+          if (res.data.length > 0) {
+            this.hasData = true
+          } else {
+            this.hasData = false
+          }
+        } else {
+          this.hasData = false;
+        }
+      }
+    )
+  }
+
+  goBtn() {
+    if (!this.fromDate || !this.toDate) {
+      return console.log("error fill all feild");
+
+    }
+    this.fetchDownloadTarget();
+  }
+
+  downloadXlSheet(){
+    
+  }
+
+}
+
+
+class DailyTargets {
+  cropNameEnglish!: string;
+  varietyNameEnglish!: string;
+  toDate!: string;
+  toTime!: string;
+  grade!: string;
+  TargetQty!: string;
+  CompleteQty!: string;
+  status!: string;
 }
--- src/app/services/Target-service/target.service.ts
+++ src/app/services/Target-service/target.service.ts
@@ -30,16 +30,28 @@ export class TargetService {
     return this.http.post<any>(url, data, { headers });
   }
 
-  getAllDailyTarget(page:number = 1, limit: number = 10, searchText:string = ''): Observable<any> {
+  getAllDailyTarget(page: number = 1, limit: number = 10, searchText: string = ''): Observable<any> {
     const headers = new HttpHeaders({
       Authorization: `Bearer ${this.token}`
     });
 
     let url = `${this.apiUrl}/get-daily-target?page=${page}&limit=${limit}`;
 
-    if(searchText){
-      url+=`&searchText=${searchText}`
+    if (searchText) {
+      url += `&searchText=${searchText}`
     }
+
+    return this.http.get<any>(url, { headers });
+  }
+
+  downloadDailyTarget(fromDate: Date | string, toDate: Date | string): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.token}`
+    });
+
+    let url = `${this.apiUrl}/download-daily-target?fromDate=${fromDate}&toDate=${toDate}`;
+
+
 
     return this.http.get<any>(url, { headers });
   }

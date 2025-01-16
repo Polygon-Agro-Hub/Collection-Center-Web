fd966b2aa4e56ec3d68caf5d088d5815ad6b6508
Pasan-FDo <pasanfernando.work@gmail.com>
2025-01-16 12:17:57 +0530

    feat: add Excel export functionality to DownloadTargetComponent

--- src/app/application/Target/download-target/download-target.component.ts
+++ src/app/application/Target/download-target/download-target.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { TargetService } from '../../../services/Target-service/target.service';
+import * as XLSX from 'xlsx';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-download-target',
@@ -50,9 +52,38 @@ export class DownloadTargetComponent {
     this.fetchDownloadTarget();
   }
 
-  downloadXlSheet(){
-    
+
+
+  downloadXlSheet() {
+    if (!this.targetArr || this.targetArr.length === 0) {
+      console.log('No data available to export.');
+      return;
+    }
+  
+    const worksheetData = this.targetArr.map((item, index) => ({
+      No: index + 1,
+      'Crop Name': item.cropNameEnglish,
+      'Variety Name': item.varietyNameEnglish,
+      Grade: item.grade,
+      'Target (kg)': item.TargetQty,
+      'Completed (kg)': item.CompleteQty,
+      Status: item.status,
+    }));
+  
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(worksheetData);
+  
+    const workbook: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Daily Targets');
+  
+    const excelBuffer: any = XLSX.write(workbook, {
+      bookType: 'xlsx',
+      type: 'array',
+    });
+  
+    const data: Blob = new Blob([excelBuffer], { type: 'application/octet-stream' });
+    saveAs(data, `Target-Report (${this.fromDate} - ${this.toDate}).xlsx`);
   }
+
 
 }
 

ff9d463616f9ad0d4ea60b11ed23cb9b915e42f3
AshanIsuranga <21it0478@itum.mrt.ac.lk>
2025-01-29 14:02:26 +0530

    view centers remove comments

--- src/app/services/Target-service/target.service.ts
+++ src/app/services/Target-service/target.service.ts
@@ -56,32 +56,6 @@ export class TargetService {
     return this.http.get<any>(url, { headers });
   }
 
-  // getCenterDetails(page: number = 1, limit: number = 10, province: string = "", district: string = "", search: string = ""): Observable<any> {
-  //   const headers = new HttpHeaders({
-  //     Authorization: `Bearer ${this.token}`
-  //   });
-
-  //   let url = `${this.apiUrl}/get-all-centers?page=${page}&limit=${limit}`
-
-  //   if (province) {
-  //     url += `&province=${province}`
-  //   }
-
-  //   if (district) {
-  //     url += `&searchText=${district}`
-  //   }
-
-  //   if (search) {
-  //     url += `&searchText=${search}`
-  //   }
-
-  //   return this.http.get(url, {
-  //     headers,
-  //   });
-  // }
-
-  /////////////
-
   getCenterDetails(province: string = '', district: string = '', search: string = '', page: number = 1, limit: number = 10): Observable<any> {
     const headers = new HttpHeaders({
         Authorization: `Bearer ${this.token}`

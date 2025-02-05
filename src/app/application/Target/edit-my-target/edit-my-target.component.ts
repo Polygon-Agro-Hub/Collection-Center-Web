5e46b6bb15180cdda471fc0bb8c48ca98cb2497c
Pasan-FDo <pasanfernando.work@gmail.com>
2025-02-05 10:21:31 +0530

    feat: add Edit My Target component with template, styles, and unit tests

---
+++ src/app/application/Target/edit-my-target/edit-my-target.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-my-target',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-my-target.component.html',
+  styleUrl: './edit-my-target.component.css'
+})
+export class EditMyTargetComponent {
+
+}

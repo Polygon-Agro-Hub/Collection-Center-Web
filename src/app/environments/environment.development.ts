b35371a30d03b87616d577f0178aace911547179
Pasan-FDo <pasanfernando.work@gmail.com>
2025-06-15 15:36:16 +0530

    fix: update API_BASE_URL for development environment

--- src/app/environments/environment.development.ts
+++ src/app/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  // API_BASE_URL: 'https://collection-center-api.agroworld.lk/agro-api/collection-center-api/api',
-     API_BASE_URL: 'http://localhost:5000/agro-api/collection-center-api/api',
+  API_BASE_URL: 'https://collection-center-api.agroworld.lk/agro-api/collection-center-api/api',
+    //  API_BASE_URL: 'http://localhost:5000/agro-api/collection-center-api/api',
 
 };

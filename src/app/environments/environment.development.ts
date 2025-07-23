4c1fdf28b3b4686ba9b6a77143045f3d63a3838e
Pasan-FDo <pasanfernando.work@gmail.com>
2025-07-23 11:33:32 +0530

    env change

--- src/app/environments/environment.development.ts
+++ src/app/environments/environment.development.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
   //prod
-  // API_BASE_URL: 'https://agroworld.lk/cc-api/agro-api/collection-center-api/api',
+  // API_BASE_URL: 'https://polygonagro.com/cc-api/agro-api/collection-center-api/api',
 
   // dev
-  // API_BASE_URL: 'https://collection-center-api.agroworld.lk/agro-api/collection-center-api/api',
+  // API_BASE_URL: 'https://collection-center-api.polygonagro.com/agro-api/collection-center-api/api',
 
   // local
    API_BASE_URL: 'http://localhost:5000/agro-api/collection-center-api/api',

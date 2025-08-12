6ba7c201325b955aa7bef1e275b7c6b27297361e
AshanIsuranga <21it0478@itum.mrt.ac.lk>
2025-08-12 08:34:04 +0530

    out for delivery component

---
+++ src/app/application/Distributed-Center/Distributed-Target/target-out-for-delivery/target-out-for-delivery.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-target-out-for-delivery',
+  standalone: true,
+  imports: [],
+  templateUrl: './target-out-for-delivery.component.html',
+  styleUrl: './target-out-for-delivery.component.css'
+})
+export class TargetOutForDeliveryComponent {
+
+}

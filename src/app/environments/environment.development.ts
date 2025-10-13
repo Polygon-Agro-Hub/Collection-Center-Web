a300366020db944aa5d12f8a7dd3fcba9d7d91b8
AshanIsuranga <21it0478@itum.mrt.ac.lk>
2025-10-13 12:35:06 +0530

    bug fix complaints

--- src/app/environments/environment.development.ts
+++ src/app/environments/environment.development.ts
@@ -4,9 +4,9 @@ export const environment = {
   // API_BASE_URL: 'https://polygonagro.com/cc-api/agro-api/collection-center-api/api',
 
   // dev
-  API_BASE_URL: 'https://collection-center-api.polygonagro.com/agro-api/collection-center-api/api',
+  // API_BASE_URL: 'https://collection-center-api.polygonagro.com/agro-api/collection-center-api/api',
 
   // local
-  //  API_BASE_URL: 'http://localhost:5000/agro-api/collection-center-api/api',
+   API_BASE_URL: 'http://localhost:5000/agro-api/collection-center-api/api',
 
 };

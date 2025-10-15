c81675b878329f5632f28b47af81cac947cd66ac
Pasan-FDo <pasanfernando.work@gmail.com>
2025-10-15 09:45:00 +0530

    service url change

--- src/app/services/distribution-complaints-service/distribution-complaints.service.ts
+++ src/app/services/distribution-complaints-service/distribution-complaints.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { TokenServiceService } from '../Token/token-service.service';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
